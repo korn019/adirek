027f740539626294fc6c652584ae331251330cbc
korn <spice_cy@hotmail.com>
2022-03-25 11:57:56 +0700

    LandingScreen

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
+import Main from "../components/mainpage/main";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <Main />
+      </Layout>
     </>
   );
 }
