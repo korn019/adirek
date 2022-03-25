9180ce058d916fa3f9312aaec8c31336fedfb9e7
korn <spice_cy@hotmail.com>
2022-03-25 12:16:46 +0700

    LandingScreen

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
-import Main from "../components/mainpage/main";
+import Main from "../components/mainpage/Main";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
