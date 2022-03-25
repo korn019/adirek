6da3068355bbccd3d146410a78ed6b3b73410131
korn <spice_cy@hotmail.com>
2022-03-25 11:37:48 +0700

    test component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
-// import Test from "../components/partial/Header";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
