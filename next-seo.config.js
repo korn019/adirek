525e3e40f57215841db3da4c286f2653a5ad9c3e
korn <spice_cy@hotmail.com>
2022-04-01 16:28:31 +0700

    s

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,18 +1,18 @@
 export const SEO = {
-  title:"Adirek - ให้ทุกวันคือการสร้างโอกาสใหม่",
-  description:"This example uses more of the available config options.",
-  canonical:"https://adirek.vercel.app/",
-  openGraph:{
+  title: "Adirek - ให้ทุกวันคือการสร้างโอกาสใหม่",
+  description: "This example uses more of the available config options.",
+  canonical: "https://adirek.vercel.app/",
+  openGraph: {
     url: "https://adirek.vercel.app/",
     title: "Open Graph Title",
     description: "Open Graph Description",
     images: [
       {
-        url: "/assets/img/logo.png",
+        url: "/assets/img/instructor.png",
         width: 300,
         height: 200,
         alt: "Og Image Alt",
-        type: "image/jpeg",
+        type: "image/png",
       },
     ],
     site_name: "SiteName",
