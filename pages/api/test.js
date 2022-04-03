28b570b7963f1e0a1d429bb65c016a4a507a959a
korn <spice_cy@hotmail.com>
2022-04-03 21:21:17 +0700

    test

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,6 @@
 import mysql from "serverless-mysql"
 
-const db = mysql({
+const connection = mysql({
   // config ค่าการเชื่อมต่อฐานข้อมูล
   config: {
     host: "localhost",
@@ -10,28 +10,37 @@ const db = mysql({
   },
 })
 
-export default function handler(req, res) {
-    // Router เวลาเรียกใช้งาน
-    console.log(req)
-    let sql = "SELECT * FROM instructor" // คำสั่ง sql
-     db.query(sql, function (error, result, fields) {
-       if (error) throw error
-       res.status(200).json(result)
-     })
-    // let query = db.query(sql, (err, results) => {
-    //   // สั่ง Query คำสั่ง sql
+connection.connect(function (err) {
+  if (err) {
+    return console.error("error: " + err.message)
+  }
+  console.log("MySQL Connection Successful.")
+})
 
-    //   if (err) throw err // ดัก error
-    //   let message = ""
-    //   if (results === undefined || results.length === 0) {
-    //     message = "ไม่พบข้อมูล"
-    //     return res.send(results)
-    //   } else {
-    //     // console.log(results)
-    //     message = "พบข้อมูล"
-    //     return res.status(200).json(results)
-    //   }
-    //   // ส่งค่ากลับไปให้ผู้ใช้
-    // })
+function getUsers(req, res) {
+  // Router เวลาเรียกใช้งาน
+  console.log(req)
+  let sql = "SELECT * FROM instructor" // คำสั่ง sql
+  connection.query(sql, function (error, result, fields) {
+    if (error) throw error
+    res.status(200).json(result)
+  })
 
+
+  // let query = db.query(sql, (err, results) => {
+  //   // สั่ง Query คำสั่ง sql
+
+  //   if (err) throw err // ดัก error
+  //   let message = ""
+  //   if (results === undefined || results.length === 0) {
+  //     message = "ไม่พบข้อมูล"
+  //     return res.send(results)
+  //   } else {
+  //     // console.log(results)
+  //     message = "พบข้อมูล"
+  //     return res.status(200).json(results)
+  //   }
+  //   // ส่งค่ากลับไปให้ผู้ใช้
+  // })
 }
+export default getUsers
