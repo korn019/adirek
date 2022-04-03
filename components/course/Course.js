78fe6debc8f627e8b5e9b72662c591f6c168ce76
korn <spice_cy@hotmail.com>
2022-04-03 17:13:19 +0700

    asd

--- components/course/Course.js
+++ components/course/Course.js
@@ -29,7 +29,7 @@ const Course = () => {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
-    })
+    }) 
       .then(function (response) {
         console.log(response)
         return response.json()
