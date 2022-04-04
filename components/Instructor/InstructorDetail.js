b4328a2a8d9efc79c7c1b6c80fb70b637c27cdbd
korn <spice_cy@hotmail.com>
2022-04-04 10:50:43 +0700

    modal

--- components/Instructor/InstructorDetail.js
+++ components/Instructor/InstructorDetail.js
@@ -1,7 +1,16 @@
-import {useState} from "react"
+import {useState,Fragment} from "react"
+import {Dialog, Transition} from "@headlessui/react"
 const InstructorDetail = ({e}) => {
   const [showModal, setShowModal] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
   return (
     <>
       <div
@@ -105,13 +114,78 @@ const InstructorDetail = ({e}) => {
                 </div>
                 <div className="align-items-center justify-content-center ">
                   <div className="title text-black ">
-                      <p
-                        className="text-black font-bold uppercase text-flg px-2 py-1 hover:text-blue-500 cursor-pointer text-left   ease-linear transition-all duration-150"
-                        onClick={() => setShowModal(true)}>
-                        อ่านเพิ่มเติม
-                      </p>
+                    <p
+                      className="text-black font-bold uppercase text-flg px-2 py-1 hover:text-blue-500 cursor-pointer text-left   ease-linear transition-all duration-150"
+                      onClick={() => setIsOpen(true)}>
+                      อ่านเพิ่มเติม
+                    </p>
                   </div>
                 </div>
+
+                <Transition show={isOpen} as={Fragment}>
+                  <Dialog
+                    as="div"
+                    className="fixed inset-0 z-10 overflow-y-auto"
+                    onClose={closeModal}>
+                    <div className="min-h-screen px-4 text-center">
+                      <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0">
+                        <Dialog.Overlay className="fixed inset-0 bg-black/[.2]" />
+                      </Transition.Child>
+
+                      {/* This element is to trick the browser into centering the modal contents. */}
+                      <span className="inline-block h-screen align-middle" aria-hidden="true">
+                        &#8203;
+                      </span>
+                      <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95">
+                        <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                          <div className="flex justify-between items-center">
+                            <Dialog.Title
+                              as="h3"
+                              className="text-lg font-medium leading-6 text-gray-900">
+                              <h3 className="text-f3xl font-title">{e.Category}</h3>
+                            </Dialog.Title>
+                            <Dialog.Title as="h3" className="text-right leading-6 text-gray-900">
+                              <button className="font-black" onClick={closeModal}>
+                                X
+                              </button>
+                            </Dialog.Title>
+                          </div>
+                          <div className="mt-2">
+                            <p className="text-sm text-gray-500">{e.รายละเอียดคอร์สเรียน}</p>
+                          </div>
+
+                          <div className="mt-4">
+                            <div className="flex justify-between items-center">
+                              <p> {parseFloat(e.ราคาคอร์สเรียน.replace(/,/g, ""))} บาท</p>
+                              <a href={`tel:${e.เบอร์โทร}`}>
+                                <button
+                                  type="button"
+                                  className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                                  onClick={closeModal}>
+                                  โทรเลย!
+                                </button>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </Transition.Child>
+                    </div>
+                  </Dialog>
+                </Transition>
 
                 {showModal ? (
                   <>
