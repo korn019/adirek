c7b06090f3d807ee38b2c4c2d4a8e5b4ec23268b
korn <spice_cy@hotmail.com>
2022-04-16 16:56:08 +0700

    fixed

--- pages/_document.js
+++ pages/_document.js
@@ -5,19 +5,20 @@ export default function Document() {
   return (
     <Html>
       <Head>
-<link rel="stylesheet" href="../static/css/bootstrap.min.css" />
-<link rel="stylesheet" href="../static/css/fontawesome.min.css" />
+        <base href="http://159.223.70.115/ " />
+        <link rel="stylesheet" href="static/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="static/css/fontawesome.min.css" />
 
-  <link rel="stylesheet" href="../static/css/animate.min.css" />
-<link rel="stylesheet" href="../static/css/magnific.min.css" />
-<link rel="stylesheet" href="../static/css/nice-select.min.css" />
-<link rel="stylesheet" href="../static/css/owl.min.css" />
-<link rel="stylesheet" href="../static/css/slick-slide.min.css" />
-<link rel="stylesheet" href="../static/css/style.css" />
-{/* <link rel="stylesheet" href="/static/sass/style.scss" /> */}
-<link rel="stylesheet" href="../static/css/responsive.css" />
-<link rel="stylesheet" href="../static/jquery.Thailand.js/dist/jquery.Thailand.min.css" />  
-     </Head>
+        <link rel="stylesheet" href="static/css/animate.min.css" />
+        <link rel="stylesheet" href="static/css/magnific.min.css" />
+        <link rel="stylesheet" href="static/css/nice-select.min.css" />
+        <link rel="stylesheet" href="static/css/owl.min.css" />
+        <link rel="stylesheet" href="static/css/slick-slide.min.css" />
+        <link rel="stylesheet" href="static/css/style.css" />
+        {/* <link rel="stylesheet" href="/static/sass/style.scss" /> */}
+        <link rel="stylesheet" href="static/css/responsive.css" />
+        <link rel="stylesheet" href="static/jquery.Thailand.js/dist/jquery.Thailand.min.css" />
+      </Head>
       <body className="sc5">
         <Main />
         <NextScript />
