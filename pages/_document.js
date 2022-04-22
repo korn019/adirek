d33eaee63356029d4cefdc6644f9215001cb6d98
korn019 <spice_cy@hotmail.com>
2022-04-22 18:22:51 +0700

    test

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import {Html, Head, Main, NextScript} from "next/document"
+
+
+export default function Document() {
+  return (
+    <Html  lang="en">
+      <Head/>
+      {/* <link rel="stylesheet" href="/static/css/bootstrap.min.css" /> */}
+      {/* <base href="http://159.223.70.115/ " />
+        <link rel="stylesheet" href="static/css/fontawesome.min.css" />
+
+        <link rel="stylesheet" href="static/css/animate.min.css" />
+        <link rel="stylesheet" href="static/css/magnific.min.css" />
+        <link rel="stylesheet" href="static/css/nice-select.min.css" />
+        <link rel="stylesheet" href="static/css/owl.min.css" />
+        <link rel="stylesheet" href="static/css/slick-slide.min.css" />
+        <link rel="stylesheet" href="static/css/style.css" />
+        <link rel="stylesheet" href="/static/sass/style.scss" /> 
+        <link rel="stylesheet" href="static/css/responsive.css" />
+        <link rel="stylesheet" href="static/jquery.Thailand.js/dist/jquery.Thailand.min.css" />  */}
+   
+      <body className="sc5">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
