c9dc420c97624a8ab15df01822a669b862ad6614
korn019 <spice_cy@hotmail.com>
2022-04-22 18:02:04 +0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -29,18 +29,7 @@ function MyApp({Component, pageProps}) {
   return (
     <>
       <DefaultSeo {...SEO} />
-      <Head>
-        {/* <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> */}
-        {/* <title>Adirek - ให้ทุกวันคือการสร้างโอกาสใหม่</title>
-         <meta name="description" content="Adirek - ให้ทุกวันคือการสร้างโอกาสใหม่" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Mitr:wght@200;300;400;500;600;700&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"/>  
-        {/* <link rel="icon" href="/public/static/img/fevicon.png" /> */}
-        {/* <link rel="preload" href="/assets/fonts/DB-Adman-X-Bd.ttf" as="font" crossOrigin="" /> */}
-      </Head>
+ 
       <Component {...pageProps} />
     </>
   )
